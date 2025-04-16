a719c464fc30dc0e107147c9a9be9c9d66ac94db
KRIN PATEL <162860250+KRIN-PATEL@users.noreply.github.com>
2025-04-16 01:42:54 -0400

    Update isAuthenticated.js

--- middlewares/isAuthenticated.js
+++ middlewares/isAuthenticated.js
@@ -3,23 +3,25 @@ import jwt from "jsonwebtoken";
 const isAuthenticated = async (req, res, next) => {
   try {
     const token = req.cookies.token;
+
     if (!token) {
       return res.status(401).json({
         message: "User not authenticated",
         success: false,
       });
     }
-    const decode = await jwt.verify(token, process.env.SECRET_KEY);
-    if (!decode) {
-      return res.status(401).json({
-        message: "Invalid token",
-        success: false,
-      });
-    }
-    req.id = decode.userId;
+
+    const decoded = jwt.verify(token, process.env.SECRET_KEY);
+
+    req.id = decoded.userId;
     next();
   } catch (error) {
-    console.log(error);
+    console.error("Auth Middleware Error:", error.message);
+    return res.status(401).json({
+      message: "Authentication failed",
+      success: false,
+    });
   }
 };
+
 export default isAuthenticated;
